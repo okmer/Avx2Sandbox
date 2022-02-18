72a0e06fd476e302ddd791f18bb4789bdc36c18d
Remko van Brakel <rvbr@allseas.com>
2022-02-18 22:18:14 +0100

    Add project files.

---
+++ Avx2Sandbox/ArrayExtension.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avx2Sandbox
+{
+    internal static class ArrayExtension
+    {
+        internal static bool IsAvx2 = Avx2.IsSupported;
+
+        internal static void ApplyInPlace_(this float[] input, MultiplicationInfo[] multiplierInfos)
+        {
+            foreach (var multiplierInfo in multiplierInfos)
+            {
+                if (input.Length <= multiplierInfo.FromInclusive)
+                {
+                    continue;
+                }
+
+                var fromInclusive = Math.Max(0, multiplierInfo.FromInclusive);
+                var toExclusive = Math.Min(input.Length, multiplierInfo.ToExclusive);
+                var multiplier = multiplierInfo.Multiplier;
+                for (var i = fromInclusive; i < toExclusive; i++)
+                {
+                    input[i] *= multiplier;
+                }
+            }
+        }
+
+        internal static void ApplyInPlace(this float[] input, MultiplicationInfo[] multiplierInfos)
+        {
+            foreach (var multiplierInfo in multiplierInfos)
+            {
+                if (input.Length <= multiplierInfo.FromInclusive)
+                {
+                    continue;
+                }
+
+                var fromInclusive = Math.Max(0, multiplierInfo.FromInclusive);
+                var toExclusive = Math.Min(input.Length, multiplierInfo.ToExclusive);
+                var multiplier = multiplierInfo.Multiplier;
+
+                if (IsAvx2)
+                    input.ApplyInPlaceAvx2(fromInclusive, toExclusive, multiplier);
+                else
+                    input.ApplyInPlace(fromInclusive, toExclusive, multiplier);
+            }
+        }
+
+        internal static void ApplyInPlace(this float[] input, int fromInclusive, int toExclusive, float multiplier)
+        {
+            for (var i = fromInclusive; i < toExclusive; i++)
+            {
+                input[i] *= multiplier;
+            }
+        }
+
+        internal static void ApplyInPlaceAvx2(this float[] input, int fromInclusive, int toExclusive, float multiplier)
+        {
+            int i = fromInclusive;
+            unsafe
+            {
+                Vector256<float> m = Vector256.Create(multiplier);
+                fixed (float* ptr = input)
+                {
+                    var vectorCount = Vector256<float>.Count;
+                    for (; i < toExclusive - vectorCount; i += vectorCount)
+                    {
+                        Vector256<float> v = Avx2.LoadVector256(ptr + i);
+                        Avx2.Store(ptr + i, Avx2.Multiply(m, v));
+                    }
+                }
+
+            }
+
+            for (; i < toExclusive; i++)
+            {
+                input[i] *= multiplier;
+            }
+        }
+    }
+
+    internal struct MultiplicationInfo
+    {
+        internal int FromInclusive { get; }
+        internal int ToExclusive { get; }
+        internal float Multiplier { get; }
+
+        internal MultiplicationInfo(int fromInclusive, int toExclusive, float multiplier)
+        {
+            FromInclusive = fromInclusive;
+            ToExclusive = toExclusive;
+            Multiplier = multiplier;
+        }
+    }
+}
---
+++ Avx2Sandbox/Program.cs
@@ -0,0 +1,61 @@
+﻿using Avx2Sandbox;
+using System.Diagnostics;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, AVX2!");
+
+int cycleSize = 100;
+int[] dataSizes = new int[]{ 10, 100, 1000, 10000, 100000, 1000000, 2000000, 5000000, 10000000 };
+
+foreach (int dataSize in dataSizes)
+{
+    var input = new float[dataSize];
+
+    for (int i = 0; i < dataSize; i++)
+    {
+        input[i] = Random.Shared.NextSingle();
+    }
+
+    var sw = new Stopwatch();
+
+    var a = input.ToArray();
+    for (int i = 0; i < cycleSize; i++)
+    {
+        a = input.ToArray();
+
+        sw.Start();
+
+        a.ApplyInPlace(0, dataSize, 1.111f);
+
+        sw.Stop();
+    }
+
+    Console.WriteLine($"Native size[{dataSize}] {sw.ElapsedTicks} ticks, {sw.ElapsedMilliseconds} mS");
+
+    sw.Reset();
+
+    var b = input.ToArray();
+    for (int i = 0; i < cycleSize; i++)
+    {
+        b = input.ToArray();
+
+        sw.Start();
+
+        b.ApplyInPlaceAvx2(0, dataSize, 1.111f);
+
+        sw.Stop();
+    }
+
+    Console.WriteLine($"Avx2 size[{dataSize}] {sw.ElapsedTicks} ticks, {sw.ElapsedMilliseconds} mS");
+
+    Console.WriteLine($"Native and AvX2 are equal: {a.SequenceEqual(b)}");
+    
+}
+
+Console.ReadLine();
+
+
+
+
